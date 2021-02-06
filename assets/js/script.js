6c93dcc8918ecf1a73da0ad41336ede588bc99b6
Samodra <38998978+saamodra@users.noreply.github.com>
2021-02-06 18:57:24 +0700

    update style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,31 @@
-$(window).scroll(function(){
-    var sticky = $('.fixed-top'),
-        scroll = $(window).scrollTop(),
-        windowWidth = $(window).width();
+$(window).scroll(function () {
+  var sticky = $('.fixed-top'),
+    scroll = $(window).scrollTop(),
+    windowWidth = $(window).width();
 
-    if(!$('body').hasClass('page-1')) {
-      if(windowWidth > 992) {
-        if(scroll >= 100) {
-          sticky.removeClass('navbar-dark').addClass('nav-sticky navbar-light')
-        } else {
-          sticky.removeClass('nav-sticky navbar-light').addClass('navbar-dark');
-        }
+  if (!$('body').hasClass('page-1')) {
+    if (windowWidth > 992) {
+      if (scroll >= 100) {
+        sticky.removeClass('navbar-dark').addClass('nav-sticky navbar-light')
+      } else {
+        sticky.removeClass('nav-sticky navbar-light').addClass('navbar-dark');
       }
     }
+  }
+});
+
+$(document).ready(function () {
+  var sticky = $('.fixed-top'),
+    windowWidth = $(window).width();
+
+  if (windowWidth <= 992) {
+    sticky.removeClass('navbar-dark');
+    sticky.addClass('nav-sticky navbar-light');
+  }
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ["<i class='fa fa-chevron-left text-white'></i>", "<i class='fa fa-chevron-right text-white'></i>"]
   });
-
-  $(document).ready(function() {
-    var sticky = $('.fixed-top'),
-        windowWidth = $(window).width();
-
-    if(windowWidth <= 992) {
-      sticky.removeClass('navbar-dark');
-      sticky.addClass('nav-sticky navbar-light');
-    }
-    $(".owl-carousel").owlCarousel({
-      loop: true,
-      margin: 10,
-      nav: true,
-      navText : ["<i class='fa fa-chevron-left text-white'></i>","<i class='fa fa-chevron-right text-white'></i>"]
-    });
-  })
+})
